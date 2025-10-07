89bf68ccdabff18218389ee91ff1fc4ea7e12364
leo1-stack <leo1.rioual@gmail.com>
2025-10-07 12:09:21 +0200

    Add files via upload

--- tp8/tp8.js
+++ tp8/tp8.js
@@ -1,49 +1,42 @@
 function addition() {
-    let a = Number(document.getElementById("t1").value);
-    let b = Number(document.getElementById("t2").value);
+    var a = Number(document.getElementById("t1").value);
+    var b = Number(document.getElementById("t2").value);
     document.getElementById("t3").value = a + b;
-    document.getElementById("t4").value = "";
 }
 
 function soustraction() {
-    let a = Number(document.getElementById("t1").value);
-    let b = Number(document.getElementById("t2").value);
+    var a = Number(document.getElementById("t1").value);
+    var b = Number(document.getElementById("t2").value);
     document.getElementById("t3").value = a - b;
-    document.getElementById("t4").value = "";
 }
 
 function multiplication() {
-    let a = Number(document.getElementById("t1").value);
-    let b = Number(document.getElementById("t2").value);
+    var a = Number(document.getElementById("t1").value);
+    var b = Number(document.getElementById("t2").value);
     document.getElementById("t3").value = a * b;
-    document.getElementById("t4").value = "";
 }
 
 function division() {
-    let a = Number(document.getElementById("t1").value);
-    let b = Number(document.getElementById("t2").value);
+    var a = Number(document.getElementById("t1").value);
+    var b = Number(document.getElementById("t2").value);
+
     if (b === 0) {
-        
         document.getElementById("t3").value = "Erreur";
     } else {
         document.getElementById("t3").value = a / b;
     }
-    document.getElementById("t4").value = "";
 }
 
 function permute() {
-    let a = document.getElementById("t1").value;
-    let b = document.getElementById("t2").value;
-    let temp = a;
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
     document.getElementById("t1").value = b;
-    document.getElementById("t2").value = temp;
+    document.getElementById("t2").value = a;
 }
 
 function parite() {
-    let x = Number(document.getElementById("t3").value);
-    if (isNaN(x)) {
-        alert("Veuillez d'abord effectuer une opération.");
-    } else if (x % 2 === 0) {
+    var x = Number(document.getElementById("t1").value);
+    if (x % 2 === 0) {
         document.getElementById("t4").value = "Pair";
     } else {
         document.getElementById("t4").value = "Impair";
